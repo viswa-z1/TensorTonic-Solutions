eb6025ce0da9a2799e5b27743cc26c119482608e
Viswanathan V <40058429+viswa-z1@users.noreply.github.com>
2026-04-01 16:30:27 +0530

    Update solution for matrix-transpose

--- matrix-transpose/matrix-transpose.py
+++ matrix-transpose/matrix-transpose.py
@@ -3,5 +3,5 @@ import numpy as np
 def matrix_transpose(A):
     A = np.array(A)
     N, M = A.shape
-    return A[:, :, None].swapaxes(0, 1).reshape(M, N)# Write your code here
+    return A[:, :, None].swapaxes(0, 1).reshape(M, N)
 pass
